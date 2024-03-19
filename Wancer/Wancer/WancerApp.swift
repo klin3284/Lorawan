0660ecbbf77417b3dcaf89e21df4bfb73381142c
Kenny Lin <lin.kenn@northeastern.edu>
2024-03-18 21:35:31 -0400

    initial commit

---
+++ Wancer/Wancer/WancerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WancerApp.swift
+//  Wancer
+//
+//  Created by Kenny Lin on 3/18/24.
+//
+
+import SwiftUI
+
+@main
+struct WancerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
