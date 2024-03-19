139020653a96ca69f97f56c2be04c0e09965cdba
Ethan <ethanliu50@gmail.com>
2024-03-19 14:38:03 -0400

    added models

--- Wancer/Wancer/Models/Group.swift
+++ Wancer/Wancer/Models/Group.swift
@@ -1,8 +1,23 @@
 //
 //  Group.swift
-//  Wancer
+//  wancer-swift
 //
-//  Created by Ethan Liu on 3/19/24.
+//  Created by Ethan Liu on 3/18/24.
 //
 
+
 import Foundation
+import SwiftData
+
+@Model
+class Group: Identifiable {
+    var id: Int
+    var name: String
+    
+    init(id: Int, name: String) {
+        self.id = id
+        self.name = name
+    }
+    
+}
+
--- Wancer/Wancer/Models/User.swift
+++ Wancer/Wancer/Models/User.swift
@@ -1,8 +1,25 @@
 //
 //  User.swift
-//  Wancer
+//  wancer-swift
 //
-//  Created by Ethan Liu on 3/19/24.
+//  Created by Ethan Liu on 3/16/24.
 //
 
 import Foundation
+import SwiftData
+
+@Model
+class User: Identifiable {
+    var id: Int
+    var firstName: String
+    var lastName: String
+    
+    init(id: Int, firstName: String, lastName: String) {
+        self.id = id
+        self.firstName = firstName
+        self.lastName = lastName
+    }
+    
+}
+
+
