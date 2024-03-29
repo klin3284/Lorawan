64570d2effa231ff73078f00707598ff0f17c59e
Ethan <ethanliu50@gmail.com>
2024-03-28 21:52:24 -0400

    add receiver ui

--- Wancer/Wancer/Views/MessagePage.swift
+++ Wancer/Wancer/Views/MessagePage.swift
@@ -3,7 +3,7 @@ import SwiftData
 
 struct ChatView: View {
     @StateObject var user: User
-    @StateObject var viewModel: ChatViewModel // View model for chat data
+    @StateObject var viewModel: ChatViewModel
     
     var body: some View {
         VStack(spacing: 16) {
@@ -24,16 +24,16 @@ struct ChatView: View {
                     .bold()
                 Spacer()
             }
-            
+
             Spacer()
-            
+
             List {
                 ForEach(viewModel.messages) { message in
                     HStack {
                         Spacer()
                         BubbleView(message: message, user: user)
                     }
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 8, trailing: 0))
                     .listRowSeparator(.hidden)
                 }
               }
@@ -43,7 +43,7 @@ struct ChatView: View {
             HStack(spacing: 16) {
                 TextField("Type your message", text: $viewModel.newMessageText )
                     .disableAutocorrection(true)
-                    .padding(8)
+                    .padding(10)
                     .background(Color.gray.opacity(0.2))
                     .cornerRadius(12)
                 
@@ -52,12 +52,13 @@ struct ChatView: View {
                 }) {
                     Image(systemName: "paperplane.fill")
                         .foregroundColor(.blue)
+                        .font(.system(size: 26))
                 }
             }
         }
         .padding()
         .onAppear {
-            viewModel.loadMessages()
+            viewModel.messages = viewModel.loadMessages()
         }
     }
 
@@ -68,92 +69,76 @@ struct BubbleView: View {
   let user: User
   
   var body: some View {
-    Text(message.text)
-      .padding()
-      .background(Color.blue)
-      .foregroundColor(.white)
-      .cornerRadius(10)
-      .fixedSize(horizontal: false, vertical: true)
-      .frame(alignment: .trailing)
+      Text(message.text)
+          .padding()
+          .background(message.author == user ? Color.blue : Color.gray.opacity(0.4))
+          .foregroundColor(message.author == user ? .white : .black)
+          .cornerRadius(10)
+          .fixedSize(horizontal: false, vertical: true)
+          .frame(maxWidth: .infinity, alignment: message.author == user ? .trailing : .leading)
   }
 }
 class ChatViewModel: ObservableObject {
     @Published var messages: [Message] = []
     @Published var newMessageText: String = ""
-    @Query private var groups: [Group]
-    @Query private var users: [User]
-    @Environment(\.modelContext) var modelContext
+    private var groups: [Group]
     private var currentGroup: Group?
-
+    private var users: [User]
+    
     func fetchGroup(with id: String) -> Group? {
         return groups.first(where: {$0.id == Int(id)})
     }
-
+    
     func fetchUser(with id: String) -> User? {
         return users.first(where: {$0.id == Int(id)})
     }
     
-    init(groupId: Int) {
+    init(groupId: Int, groups: [Group], users: [User]) {
+        self.groups = groups
+        self.users = users
         self.currentGroup = groups.first(where: { $0.id == groupId })
     }
-
+    
     func loadMessages() -> [Message] {
         if let currentGroup {
-            return currentGroup.messages
+            return currentGroup.messages.sorted(by: { $0.createdAt < $1.createdAt })
         }
         return []
     }
     
     func sendMessage(user: User) {
-        let msg : Message = Message(id: messages.count + 1, text: newMessageText, createdAt: Date.now, author: user, seen: false, group: currentGroup!)
-        
-        messages.append(msg)
-        newMessageText = ""
+        if !newMessageText.isEmpty {
+            if let fetchedUser = fetchUser(with: String(user.id)) {
+                let msg = Message(id: messages.count + 1, text: newMessageText, createdAt: Date.now, author: fetchedUser, seen: false, group: currentGroup!)
+                messages.append(msg)
+                newMessageText = ""
+            } else {
+                print("Error fetching user for message")
+            }
+        }
     }
 }
 
 #Preview {
-    if let container = try? ModelContainer(for: Message.self, User.self, Group.self) {
-        let container = try? ModelContainer(for: Message.self, User.self, Group.self)
-        let user = User(id: 104, firstName: "Ethan", lastName: "Liu", groups: [])
+    let config = ModelConfiguration(isStoredInMemoryOnly: true)
 
-        let group = Group(id: 204, name: "group", users: [user], messages: [])
-        
-        container!.mainContext.insert(user)
-        container!.mainContext.insert(group)
-        
-        user.groups.append(group)
-        
-        let message1 = Message(id: 300, text: "Hello!", createdAt: Date.now, author: user, seen: false, group: group)
-        let message2 = Message(id: 400, text: "Hi!", createdAt: Date.now, author: user, seen: true, group: group)
-        group.addMessage(message1)
-        group.addMessage(message2)
+    let container = try! ModelContainer(for: Message.self, User.self, Group.self, configurations: config)
+    let user = User(id: 104, firstName: "Ethan", lastName: "Liu", groups: [])
+    let user1 = User(id: 103, firstName: "John", lastName: "Smith", groups: [])
 
-        do {
-            try container!.mainContext.save()
-        } catch {
-            print("Error saving context: \(error)")
-        }
+    let group = Group(id: 204, name: "group", users: [user], messages: [])
         
-        do {
-            try container?.mainContext.delete(model: User.self)
-        }
-        catch {
-            print("Error saving context: \(error)")
-        }
+    container.mainContext.insert(user)
+    container.mainContext.insert(group)
         
-        do {
-            try container?.mainContext.delete(model: Group.self)
-        }
-        catch {
-            print("Error saving context: \(error)")
-        }
+    user.groups.append(group)
+
+    let message1 = Message(id: 300, text: "Helloooo!", createdAt: Date.now, author: user1, seen: false, group: group)
+    let message2 = Message(id: 400, text: "Hi!", createdAt: Date.now.addingTimeInterval(1000), author: user1, seen: true, group: group)
+    group.addMessage(message1)
+    group.addMessage(message2)
     
-        return ChatView(user: user, viewModel: ChatViewModel(groupId: group.id))
-        
-     } else {
-       print("Error creating container")
-       return AnyView(EmptyView())
-     }
+    return ChatView(user: user, viewModel: ChatViewModel(groupId: group.id, groups: [group], users: [user]))
+        .modelContainer(container)
 }
 
