c2c3aa4c37310ecbbffce426317d3170069b92d3
Kenny Lin <lin.kenn@northeastern.edu>
2024-03-30 23:52:20 -0400

    merge from main

--- Wancer/Wancer/Models/Group.swift
+++ Wancer/Wancer/Models/Group.swift
@@ -19,9 +19,9 @@ class Group: Identifiable {
     var acceptedAt: Date?
     
     @Relationship(deleteRule: .nullify)
-    var users: [User]
+    var users: [User]?
     
-    @Relationship(deleteRule: .cascade)
+    @Relationship(deleteRule: .cascade, inverse: \Message.group)
     var messages: [Message]
     
     init(id: Int, name: String, users: [User], messages: [Message]) {
@@ -41,7 +41,7 @@ class Group: Identifiable {
     }
     
     func addUser(_ user: User) {
-        users.append(user)
+        users = (users ?? []) + [user]
     }
     
     func addMessage(_ message: Message) {
@@ -49,8 +49,9 @@ class Group: Identifiable {
     }
     
     func removeUser(_ user: User) {
-        if let index = users.firstIndex(where: { $0.id == user.id }) {
+        if var users = users, let index = users.firstIndex(where: { $0.id == user.id }) {
             users.remove(at: index)
+            self.users = users
         }
     }
     
