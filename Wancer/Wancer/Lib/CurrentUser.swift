7e87641bacddfa2b44448f9486e82ce8db18bfbd
Kenny Lin <lin.kenn@northeastern.edu>
2024-04-01 02:58:09 -0400

    initial

---
+++ Wancer/Wancer/Lib/CurrentUser.swift
@@ -0,0 +1,18 @@
+//
+//  CurrentUser.swift
+//  Wancer
+//
+//  Created by Kenny Lin on 4/1/24.
+//
+
+import SwiftUI
+
+struct CurrentUser: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CurrentUser()
+}
